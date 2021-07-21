b35388a62381f60c5cfa4b6a8cb1d94ee26d9da9
jleiandy <jleiandy@gmail.com>
2021-07-21 03:44:05 -0700

    Passed tests 1-10;

--- src/recursion.js
+++ src/recursion.js
@@ -7,31 +7,108 @@
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
 var factorial = function(n) {
+  if(n < 0) {
+    return null;
+  }
+
+  if( n == 0) {
+  return 1;
+}
+return result  = n*(factorial(n-1));
+
 };
 
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+
+var duplicateArray = array.slice();
+
+  if(duplicateArray.length === 0) {
+    return 0;
+  }
+
+  return duplicateArray.pop() + sum(duplicateArray);
+
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
+
+  var accumulator = 0;
+
+  if(!Array.isArray(array)) {
+    return array
+  };
+
+  array.forEach(function(item) {
+    accumulator += arraySum(item);
+  });
+
+  return accumulator;
+
 };
 
 // 4. Check if a number is even.
 var isEven = function(n) {
+  if(n === 1 || n === -1) {
+    return false;
+  }
+  if (n === 0) {
+    return true;
+  }
+
+  if(n > 0) {
+  return isEven(n-2);
+  }
+
+  if(n < 0) {
+    return isEven(n+2);
+  }
 };
 
 // 5. Sum all integers below a given integer.
 // sumBelow(10); // 45
 // sumBelow(7); // 21
 var sumBelow = function(n) {
+
+  if(n === 0) {
+    return 0;
+  }
+
+  if(n > 0) {
+    return (n-1) + (sumBelow(n-1));
+  }
+
+  if(n < 0) {
+    return (n + 1) + (sumBelow(n+1));
+  }
+
 };
 
 // 6. Get the integers within a range (x, y).
 // range(2,9); // [3,4,5,6,7,8]
 var range = function(x, y) {
+
+var rangeArr = [];
+
+if(y >= x) {
+  if (Math.abs(y - x) === 1 || Math.abs(y - x) === 0) {
+    return [];
+  }
+
+  return rangeArr.concat(x + 1).concat(range(x + 1, y));
+}
+
+if(y <= x) {
+  if (Math.abs(x - y) === 1 || Math.abs(y - x) === 0) {
+    return [];
+  }
+
+  return rangeArr.concat(x - 1).concat(range(x - 1, y));
+}
+
 };
 
 // 7. Compute the exponent of a number.
@@ -40,6 +117,23 @@ var range = function(x, y) {
 // exponent(4,3); // 64
 // https://www.khanacademy.org/computing/computer-science/algorithms/recursive-algorithms/a/computing-powers-of-a-number
 var exponent = function(base, exp) {
+  if(exp === 0) {
+    return 1;
+  }
+  if (exp % 2 === 0) {
+    var y = (exponent(base, exp/2))
+    return Number((y*y).toFixed(5));
+  }
+
+  if(exp > 0) {
+  return Number(((base) * (exponent(base, exp - 1))).toFixed(5));
+  }
+
+  if(exp < 0) {
+    return Number(((1/base) * (exponent(base, exp + 1))).toFixed(5));
+    }
+
+
 };
 
 // 8. Determine if a number is a power of two.
@@ -47,14 +141,65 @@ var exponent = function(base, exp) {
 // powerOfTwo(16); // true
 // powerOfTwo(10); // false
 var powerOfTwo = function(n) {
+
+  if(n === 1) {
+    return true;
+  }
+
+  if(n < 1) {
+    return false;
+  }
+
+  return powerOfTwo(n/2);
+
 };
 
 // 9. Write a function that reverses a string.
 var reverse = function(string) {
+  var revStr = ''
+
+  if(string.length === 1) {
+    return string;
+  }
+
+  return revStr += (string.slice(string.length-1)) + reverse(string.slice(0, string.length - 1));
+
 };
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+var noSpaceNoCaps = string.split(" ").join("").toLowerCase();
+  if (string.length === 1) {
+  return true;
+  }
+
+  if(noSpaceNoCaps.length % 2 === 0) {
+    if(noSpaceNoCaps.length === 2) {
+      return noSpaceNoCaps[0] === noSpaceNoCaps[1];
+    }
+    var frontChar = noSpaceNoCaps.slice(0, 1);
+    var endChar = noSpaceNoCaps.slice(noSpaceNoCaps.length -1);
+    if (frontChar === endChar) {
+      return palindrome(noSpaceNoCaps.slice(1, noSpaceNoCaps.length - 1))
+    } else {
+      return false;
+    }
+  }
+
+  if(noSpaceNoCaps.length % 2 === 1) {
+    if(noSpaceNoCaps.length === 3) {
+      return noSpaceNoCaps[0] === noSpaceNoCaps[2];
+    }
+    var frontChar = noSpaceNoCaps.slice(0, 1);
+    var endChar = noSpaceNoCaps.slice(noSpaceNoCaps.length -1);
+    if (frontChar === endChar) {
+      return palindrome(noSpaceNoCaps.slice(1, noSpaceNoCaps.length - 1))
+    } else {
+      return false;
+    }
+  }
+
+
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the
@@ -136,6 +281,8 @@ var countKeysInObj = function(obj, key) {
 // countValuesInObj(obj, 'r') // 2
 // countValuesInObj(obj, 'e') // 1
 var countValuesInObj = function(obj, value) {
+  var counter = 0;
+
 };
 
 // 24. Find all keys in an object (and nested objects) by a provided name and rename
